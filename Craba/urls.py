b6b1f69d27ac24eaaff7e4251998d538de7902de
mael-donnadille <mael.donnadille@e.rascol.net>
2024-02-02 15:30:35 +0100

    Modif base.html

--- Craba/urls.py
+++ Craba/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 
 from Craba import views
-from Craba.views import home
+from .views import page_entre, page_carte
+
 
 urlpatterns = [
     path('', views.home),
+    path('entreprise/', page_entre, name='entreprise'),
+    path('carte/', page_carte, name='carte'),
+
+
 ]
--- Craba/views.py
+++ Craba/views.py
@@ -1,5 +1,16 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+#from .models import Lieu
 def home(request):
    return render(request, 'accueil.html')
+
+
+def page_entre(request):
+   return render(request, 'enterprise.html')
+
+def page_carte(request):
+   return render(request, 'carte.html')
+
+
+
+
